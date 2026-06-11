0b60f1eafe249430e9bc305aa01fb6d2ca02f075
libing-hong <libing.hong@kedgebs.com>
2026-06-11 17:59:31 +0200

    Add offline cache worker

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+const cacheName = "couple-fat-loss-v3";
+const assets = ["./", "./index.html", "./manifest.json", "./icon.svg"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(assets)));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== cacheName).map((key) => caches.delete(key)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+});
